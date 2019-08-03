206086b8b583b3df0c614c7d0233df5bb6b4af98
Rahul Kumar <rahulkkumar1997@gmail.com>
2019-08-03 23:59:43 +0530

    Add files via upload

---
+++ disarium.java
@@ -0,0 +1,34 @@
+import java.util.*;
+class disarium
+{
+public static void main(String args[])
+{
+Scanner sc =new Scanner(System.in);
+int a,n=0,c=0,b=0;
+double s=0;
+System.out.println("Enter a number to check for disarium");
+a=sc.nextInt();
+n=a;
+while(n!=0)
+{
+c++;
+n=n/10;
+}
+n=a;
+for(int i=c;i>0;i--)
+{
+	b=n%10;
+	s=s+Math.pow(b,i);
+	n=n/10;
+}
+
+if(a==s)
+{
+System.out.println(a+" is a disarium number");
+}
+else
+{
+	System.out.println(a+" is not a disarium number");
+}
+}
+}
